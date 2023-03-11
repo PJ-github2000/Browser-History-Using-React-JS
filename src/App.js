665176e68ec132eb35a6d86bf7cf9b645b49e078
praveenm <gpraveen1542@gmail.com>
2023-03-11 06:12:56 +0000

    history

---
+++ src/App.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import HistorySearch from './components/HistorySearch'
+import './App.css'
+
+const App = () => <HistorySearch />
+
+export default App
